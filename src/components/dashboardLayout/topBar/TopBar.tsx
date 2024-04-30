e7cb2dc9227a425521fabf02554aa424576920e2
Edyta2801 <edyta.szarowska@interia.pl>
2024-04-30 12:34:46 +0200

    fix bug with close Menu

--- src/components/dashboardLayout/topBar/TopBar.tsx
+++ src/components/dashboardLayout/topBar/TopBar.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import {
   AppBar,
   Avatar,
@@ -12,24 +12,40 @@ import {
 import * as styles from './TopBar.styles';
 
 export function TopBar() {
+  const avatarRef = useRef<HTMLDivElement | null>(null);
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const onMouseEnter = useCallback((event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   }, []);
-
-  const handleClose = useCallback(() => {
+  const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', (event) => {
+      if (!avatarRef.current) return;
+
+      const offset = 250;
+      const { top, right, bottom, left } =
+        avatarRef.current.getBoundingClientRect();
+
+      if (event.clientX < left - offset) setAnchorEl(null);
+      if (event.clientY < top - offset) setAnchorEl(null);
+      if (event.clientX > right + offset) setAnchorEl(null);
+      if (event.clientY > bottom + offset) setAnchorEl(null);
+    });
   }, []);
   return (
     <AppBar sx={styles.topBar}>
-      {' '}
       <Toolbar sx={styles.wrapper}>
         <Typography variant="h6" component="div">
           HR_Analytics
-        </Typography>{' '}
-        <Box onMouseEnter={onMouseEnter} onMouseLeave={handleClose}>
+        </Typography>
+
+        <Box onMouseEnter={onMouseEnter} ref={avatarRef}>
           <Avatar>ES</Avatar>
           <Menu
+            onClose={handleClose}
             anchorEl={anchorEl}
             open={Boolean(anchorEl)}
             anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
